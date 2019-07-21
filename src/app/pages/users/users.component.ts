3fd56dc275a73a05c13374454c9226fbdd0a18e3
mohammadobaid1 <mobaid56@gmail.com>
2019-07-21 12:48:14 +0500

    some fixes

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -3,8 +3,8 @@ import { BasePageComponent } from '../base-page';
 import { Store } from '@ngrx/store';
 import { IAppState } from '../../interfaces/app-state';
 import { HttpService } from '../../services/http/http.service';
-import { TCModalService } from '../../../ui/services/modal/modal.service';
-import { Content } from '../../../ui/interfaces/modal';
+import { TCModalService } from '../../ui/services/modal/modal.service';
+import { Content } from '../../ui/interfaces/modal';
 import {Router} from "@angular/router";
 
 
